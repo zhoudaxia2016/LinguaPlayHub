5eef13cf680673467f0eca40aabdd11c308f2b1a
zhoudaxia <muzhouz@gmail.com>
2024-04-20 15:32:59 +0800

    feat(client): 样式调整，抽取css变量

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,36 @@ import WordSearch from './WordSearch'
 import Read from './Read'
 import Nav from './Nav'
 import {Routes, Route} from "react-router-dom"
+import {ConfigProvider} from 'antd'
+import Palette from './Palette'
+
+const cssVars = Object.keys(Palette).reduce((vars, key) => {
+  vars[`--${key}`] = Palette[key]
+  return vars
+}, {})
 
 export default function App() {
   return (
-    <div className="app">
-      <a className="app-title" href="/">LanguaPlayHub</a>
-      <div className="app-content">
-        <Routes>
-          <Route path="/" element={<Nav/>}/>
-          <Route path="/wordsearch" element={<WordSearch/>}/>
-          <Route path="/read" element={<Read/>}/>
-        </Routes>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorBgBase: Palette.bg1,
+          colorText: Palette.fg1,
+          colorPrimary: Palette.stress2,
+          controlOutline: Palette.stress2,
+        },
+      }}
+    >
+      <div className="app" style={cssVars}>
+        <a className="app-title" href="/">LanguaPlayHub</a>
+        <div className="app-content">
+          <Routes>
+            <Route path="/" element={<Nav/>}/>
+            <Route path="/wordsearch" element={<WordSearch/>}/>
+            <Route path="/read" element={<Read/>}/>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </ConfigProvider>
   )
 }
---
+++ client/src/Palette.ts
@@ -0,0 +1,8 @@
+export default {
+  bg1: '#dabba9',
+  bg2: '#8c4e37',
+  fg1: '#402722',
+  fg2: '',
+  stress1: '#d9383a',
+  stress2: '#a73736',
+}
