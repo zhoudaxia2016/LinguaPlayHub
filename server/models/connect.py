522f5a46d0458bd3832c188ef7ede6c020346361
zhoudaxia <muzhouz@gmail.com>
2024-04-25 00:36:04 +0800

    feat: 【词汇】脚本导入编码问题，mysql会将クラス　グラス当作相同

--- server/models/connect.py
+++ server/models/connect.py
@@ -5,4 +5,4 @@ def connect():
     user = os.environ.get('user')
     password = os.environ.get('password')
     database = 'LinguaPlayHub'
-    return create_engine(f'mysql+pymysql://{user}:{password}@127.0.0.1/{database}', echo=True, max_overflow=5)
+    return create_engine(f'mysql+pymysql://{user}:{password}@127.0.0.1/{database}?charset=utf8mb4', echo=True, max_overflow=5)
--- server/scripts/import_vocab.py
+++ server/scripts/import_vocab.py
@@ -6,13 +6,18 @@ from routers.utils import get_db
 
 fn = sys.argv[1]
 sheet = pd.read_excel(fn, sheet_name=0)
+word_set = set()
 
 db = next(get_db())
-for i in range(1, 500):
+for i in range(1, len(sheet)):
     name = sheet.iloc[i, 0]
-    if db.query(Vocab).filter_by(name=name).count() > 0:
+    if name in word_set or pd.isna(name) or db.query(Vocab).filter_by(name=name).count() > 0:
+        word_set.add(name)
         continue
+    word_set.add(name)
     kana = sheet.iloc[i, 1]
+    if pd.isna(kana):
+        kana = ''
     status = 100 if sheet.iloc[i, 2] else 10
     db.add(Vocab(name=name, kana=kana, status=status))
 
