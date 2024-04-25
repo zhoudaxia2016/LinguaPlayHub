f1bdec923c09fd89535c084558940d20b201b90f
zhoudaxia <muzhouz@gmail.com>
2024-04-26 00:46:47 +0800

    feat: 【词汇】支持筛选，排序

--- client/src/Vocab/index.tsx
+++ client/src/Vocab/index.tsx
@@ -1,14 +1,60 @@
 import './index.less'
 import React, {useCallback, useEffect, useMemo, useState} from 'react'
-import {Table, Progress, Space, Button} from 'antd'
+import {Table, Progress, Space, Button, Input, Slider, Radio} from 'antd'
 import {getVocab, deleteWord, updateWord} from './api'
 
 interface IWord {
   id: number,
+  kana: string,
   name: string,
   status: number,
   create_date: string,
   finish_date: string,
+}
+
+const textFilter = ({ setSelectedKeys, selectedKeys, confirm}) => (
+  <div style={{ padding: 8 }}>
+    <Input
+      allowClear
+      placeholder="输入假名|汉字"
+      value={selectedKeys[0]}
+      onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+      onPressEnter={() => confirm()}
+      style={{ width: 130, marginRight: 10 }}
+    />
+    <Button
+      type="primary"
+      onClick={() => confirm()}
+      size="small"
+      style={{ width: 60 }}
+    >
+      确定
+    </Button>
+  </div>
+)
+
+const progressFilter = ({ setSelectedKeys, selectedKeys, confirm }) => {
+    return (
+    <div style={{padding: 8}}>
+      <Slider
+        min={1}
+        max={100}
+        onChange={val => setSelectedKeys([{...selectedKeys[0], num: val}]) && confirm()}
+        style={{width: 130}} />
+      <Button
+        type="primary"
+        onClick={() => confirm()}
+        size="small"
+        style={{width: 60}}
+      >
+        确定
+      </Button>
+      <Radio.Group style={{marginLeft: 8}} onChange={e => setSelectedKeys([{...selectedKeys[0], isGt: e.target.value === 0}])}>
+        <Radio value={0}>&gt;</Radio>
+        <Radio value={1}>&lt;</Radio>
+      </Radio.Group>
+    </div>
+  )
 }
 
 export default function Vocab() {
@@ -30,11 +76,31 @@ export default function Vocab() {
       fetchData()
     }
     return [
-      {title: '单词', dataIndex: 'name'},
+      {
+        title: '单词', dataIndex: 'name',
+        filters: [
+          {
+            text: 'London',
+            value: 'London',
+          },
+          {
+            text: 'New York',
+            value: 'New York',
+          },
+        ],
+        filterDropdown: textFilter,
+        onFilter: (value, {name, kana}) => name.includes(value) || kana.includes(value),
+      },
       {title: '假名', dataIndex: 'kana'},
-      {title: '认识程度', dataIndex: 'status', render: (_, {status}) => <Progress type="circle" percent={status} size={30} />},
-      {title: '创建日期', dataIndex: 'create_date'},
-      {title: '完成日期', dataIndex: 'finish_date'},
+      {
+        title: '认识程度', dataIndex: 'status',
+        render: (_, {status}) => <Progress type="circle" percent={status} size={30} />,
+        filterDropdown: progressFilter,
+        onFilter: ({num = 0, isGt = true}: any, {status}) => isGt ? status >= num : status <= num,
+        sorter: (a, b) => a.status - b.status,
+      },
+      {title: '创建日期', dataIndex: 'create_date', sorter: (a, b) => a.status - b.status},
+      {title: '完成日期', dataIndex: 'finish_date', sorter: (a, b) => a.status - b.status},
       {title: '操作', key: 'action', render: (
         (_, {id, status}) =>
           <Space>
@@ -62,19 +128,19 @@ export default function Vocab() {
   return (
     <div className="vocab">
       <Table columns={columns} dataSource={data} rowKey="id" pagination={{pageSize: 12}}
-             sticky
-             summary={() =>
-              <Table.Summary fixed="top">
-                <Table.Summary.Row>
-                  <Table.Summary.Cell index={0}>
-                    <span className="vocab-summary-item">总数: {count}</span>
-                  </Table.Summary.Cell>
-                  <Table.Summary.Cell index={1}>
-                    <span className="vocab-summary-item">已掌握: {finishCount}</span>
-                  </Table.Summary.Cell>
-                </Table.Summary.Row>
-              </Table.Summary>
-             }
+        sticky
+        summary={() =>
+          <Table.Summary fixed="top">
+            <Table.Summary.Row>
+              <Table.Summary.Cell index={0}>
+                <span className="vocab-summary-item">总数: {count}</span>
+              </Table.Summary.Cell>
+              <Table.Summary.Cell index={1}>
+                <span className="vocab-summary-item">已掌握: {finishCount}</span>
+              </Table.Summary.Cell>
+            </Table.Summary.Row>
+          </Table.Summary>
+        }
       />
     </div>
   )
